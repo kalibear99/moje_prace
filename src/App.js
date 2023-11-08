df28ea23b4a7a934cdb2a59a036866a7cf6f37f4
Kalist Darien <2021KalistDarien@skolabaltaci.local>
2023-11-08 12:10:02 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavigationMenu from "./components/navigation_menu";
+import Portfolio from "./pages/portfolio";
+
+function App() {
+    return (
+        <NavigationMenu></NavigationMenu>,
+            <Portfolio/>
+    );
+}
+
+export default App;
---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Portfolio() {
+    return (
+        <div>
+            <section id="section1">
+                <h2>Sekce 1</h2>
+                {/* Obsah sekce 1 */}
+            </section>
+            <section id="section2">
+                <h2>Sekce 2</h2>
+                {/* Obsah sekce 2 */}
+            </section>
+            <section id="section3">
+                <h2>Sekce 3</h2>
+                {/* Obsah sekce 3 */}
+            </section>
+        </div>
+    );
+}
+export default Portfolio;
+
